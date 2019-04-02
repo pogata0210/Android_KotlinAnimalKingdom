39703136ecf72ea9ae4f54898f56354e1e14562b
Pablo Ogata <pabloogata@gmail.com>
2019-04-02 06:54:29 -0400

    new

--- src/AnimalClass.kt
+++ src/AnimalClass.kt
@@ -1,22 +1,31 @@
-abstract class AnimalClass(
+abstract class AnimalClass(val id : Int, val name: String, val year: Int) {
+    fun consumeFood(){
+        println("$name consume food")
+    }
+    //, var canMove: canMove, var canBreath: canBreath, val canReproduce: canReproduce
 
-    val id : Int,
-    val name: String,
-    val year: Int,
-    val canMove: canMove,
-    val canBreath: canBreath,
-    val canReproduce: canReproduce
-
-)
-
-class canBreath {
-
+    abstract fun canMove(): String
+    abstract fun canBreath(): String
+    abstract fun canReproduce(): String
 }
 
-class canMove {
-
+sealed class Move(animalName: String){
+    abstract fun performMove(animalName: String): String
 }
 
-class canReproduce {
+class Walk(animalName: String): Move(animalName) {
+    override fun performMove(animalName: String): String {
+        return "$animalName walks"
+    }
 
+
+    class Mammal(id: Int, name: String, year: Int) : AnimalClass(id, name, year) {
+        override fun canMove(): String {
+            return Walk(name).performMove(name)
+        }
+
+        override fun canBreath() = "Breathe with lungs"
+
+        override fun canReproduce() = "Live births"
+    }
 }
