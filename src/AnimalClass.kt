a520bb38b6aba4729cddfd4517101d5f2d11414b
Pablo Ogata <pabloogata@gmail.com>
2019-04-02 06:24:29 -0400

    value

---
+++ src/AnimalClass.kt
@@ -0,0 +1,22 @@
+abstract class AnimalClass(
+
+    val id : Int,
+    val name: String,
+    val year: Int,
+    val canMove: canMove,
+    val canBreath: canBreath,
+    val canReproduce: canReproduce
+
+)
+
+class canBreath {
+
+}
+
+class canMove {
+
+}
+
+class canReproduce {
+
+}
